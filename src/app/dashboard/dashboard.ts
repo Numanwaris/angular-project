c153e202298c974df97b2b4469f6c71aa83e4e78
Numan Waris <numanwariskhan72@gmail.com>
2026-03-04 03:15:48 +0530

    Updated dashboard, home UI and styling

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -17,6 +17,8 @@ export class DashboardComponent implements OnInit {
    selectedMenu: string = 'Home';
    isEditMode: boolean = false;
    homeImages: string[] = [];
+   userAnswers: string[] = [];
+
   constructor(private auth: Auth, private router: Router) {}
 
   ngOnInit(): void {
@@ -28,7 +30,6 @@ export class DashboardComponent implements OnInit {
   }
 
   this.user = JSON.parse(userData);
-   this.homeImages = JSON.parse(localStorage.getItem('homeImages') || '[]');
 }
 onImageUpload(event: any) {
   const file = event.target.files[0];
@@ -59,12 +60,69 @@ onHomeImageUpload(event: any) {
 
   reader.readAsDataURL(file);
 }
-deleteImage(index: number) {
-  this.homeImages.splice(index, 1);
+deleteImage(index: number) 
+{ this.homeImages.splice(index, 1);
+   localStorage.setItem('homeImages', JSON.stringify(this.homeImages)); 
+  }
 
-  // Update localStorage
-  localStorage.setItem('homeImages', JSON.stringify(this.homeImages));
+  quizQuestions = [
+    { question: "What is the capital of India?", 
+      options: ["Delhi", "Mumbai", "Chennai", "Kolkata"],
+       answer: "Delhi" }, 
+
+
+       { question: "2 + 2 = ?",
+         options: ["3", "4", "5", "6"], 
+         answer: "4" }, 
+
+         { question: "Angular is a ?",
+           options: ["Framework", "Language", "Database", "Browser"], 
+           answer: "Framework" }, 
+
+           { question:"3 +2 = ?", 
+            options :["5", "2", "4", "7"], 
+            answer:"5" },
+
+            {
+              question:"CM of Up =?",
+              options:["Akhileash", "Yogitakla","Rajpal","Ramesh"],
+              answer:"Yogitakla"
+            }
+
+  ];
+    currentQuestionIndex = 0;
+    score = 0;
+    wrong = 0;
+    selectedOption = '';
+    quizFinished = false;
+
+  selectOption(option: string) {
+   this.selectedOption = option;
 }
+        
+    nextQuestion(){
+      if (this.selectedOption === this.quizQuestions[this.currentQuestionIndex].answer) {
+     this.score++;
+}    else {
+     this.wrong++;
+}      this.selectedOption = '';
+
+      if (this.currentQuestionIndex < this.quizQuestions.length - 1) {
+  this.currentQuestionIndex++;
+} else {
+  this.quizFinished = true;
+}
+    } 
+    restartQuiz() { 
+      this.currentQuestionIndex = 0; 
+      this.score = 0; 
+      this.wrong = 0; 
+      this.quizFinished = false;
+     }
+    
+
+
+
   logout() {
     localStorage.removeItem('loggedInUser');
     this.router.navigate(['/login']);
